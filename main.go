015511ac81fa0ce72eab82fb772cb061da324c29
Miguel Abad Perez <mabadper@mabadper.remote.csb>
2021-12-24 14:34:46 +1100

    First commit

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"strings"
+
+	"k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// InvalidMessage will be return to the user.
+	InvalidMessage = "Namespace modification forbidden"
+	port           = ":8443"
+)
+
+var (
+	tlscert, tlskey string
+)
+
+// Namespace struct for parsing
+type Namespace struct {
+	Metadata Metadata `json:"metadata"`
+}
+
+// Metadata struct for parsing
+type Metadata struct {
+	Name   string            `json:"name"`
+	Labels map[string]string `json:"labels"`
+}
+
+func (m Metadata) isEmpty() bool {
+	return m.Name == ""
+}
+
+// Validate handler accepts or rejects based on request contents
+func NSValidate(w http.ResponseWriter, r *http.Request) {
+	arReview := v1beta1.AdmissionReview{}
+	if err := json.NewDecoder(r.Body).Decode(&arReview); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} else if arReview.Request == nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	raw := arReview.Request.Object.Raw
+
+	ns := Namespace{}
+	if err := json.Unmarshal(raw, &ns); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} else if ns.Metadata.isEmpty() {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	arReview.Response = &v1beta1.AdmissionResponse{
+		UID:     arReview.Request.UID,
+		Allowed: true,
+	}
+
+	if strings.HasPrefix(ns.Metadata.Name, "openshift") {
+		arReview.Response.Allowed = false
+		arReview.Response.Result = &metav1.Status{
+			Message: InvalidMessage,
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&arReview)
+}
+
+func main() {
+	flag.StringVar(&tlscert, "tlsCertFile", "/etc/certs/tls.crt",
+		"File containing a certificate for HTTPS.")
+	flag.StringVar(&tlskey, "tlsKeyFile", "/etc/certs/tls.key",
+		"File containing a private key for HTTPS.")
+	flag.Parse()
+
+	http.HandleFunc("/namespace-validation", NSValidate)
+	log.Fatal(http.ListenAndServeTLS(port, tlscert, tlskey, nil))
+}
